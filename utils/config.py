f2826b1f7179e0827cf8696c03857a1fe07cfb13
nraju316 <nraju316@gmail.com>
2026-05-26 20:24:57 +0530

    conftest changes

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,11 @@
 BASE_URL = "https://fakestoreapi.com"
 
 HEADERS = {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0"
 }
 
 VALID_USERNAME = "mor_2314"
 VALID_PASSWORD = "83r5^_"
+
+
